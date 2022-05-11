4f92f3d2cc818a32b012bafa28b40e75ee9013e7
Vivian Nguyen <vivian.nguyen@go1.com>
2022-05-11 16:51:23 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="Go1 Coaching Platform">
+      <header className="go1-coaching-platform">
+        <a href="https://forms.office.com/pages/responsepage.aspx?id=p7WWmkbxWEOZz3v13HjOw3SJIa5oiBNDlPCy23W9S9xUMlQwUVhMUlo0VVI2NFRVSEhJUjJaNUpXMS4u">
+          <img
+            src="https://res.cloudinary.com/gohkt/image/upload/v1652250995/go1-hackathon/go1Coaching_Landing_Page_nvl1zt.png"
+            className="App-logo"
+            alt="logo"
+          />
         </a>
       </header>
     </div>
